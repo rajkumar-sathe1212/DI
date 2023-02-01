ee5fb44d23bae211bc289b90855e32221e43b244
Rajkumar Sathe <satheraj1400@gmail.com>
2023-02-01 15:48:19 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'dependancy_Injection';
+export class AppComponent implements OnInit{
+
+constructor(private api:ApiService) { }
+products:any;
+
+ngOnInit(): void {
+    this.api.get("https://fakestoreapi.com/products").subscribe((result)=>{
+      console.log(result);
+      this.products = result;
+    })
+  }
+
 }
